6b312bae3a09b42b97b54f2a8d7a6cfb1f92964d
Miles Reid <wheatleymjr@gmail.com>
2022-07-22 18:57:49 +1000

    RPSLS1

---
+++ randomstuff.py
@@ -0,0 +1,8 @@
+import random
+
+randomNumber = random.randint(1, 100)
+
+if(randomNumber > 50):
+    print("its bigger than 50 see i did some4thing")
+else:
+    print("its smaller than 50 see i did something different")
--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,16 +1,70 @@
-from asyncore import loop
+import random
+import time
 
+def createAnswer():
+    randomNumber = random.randint(1, 5)
+
+    if (randomNumber == 1):
+        return("Rock")
+
+    elif (randomNumber == 2):
+        return("Paper")
+
+    elif (randomNumber == 3):
+        return("Scissors")
+
+    elif (randomNumber == 4):
+        return("Lizard")
+
+    elif (randomNumber == 5):
+        return("Spock")
+
+def computerAnswer(userAnswer):
+    randomNumber = random.randint(0, 1)
+    
+    if userAnswer == "Rock":
+        if(randomNumber == 1):
+            print("Paper wraps Rock")
+        else:
+            print("Spock vaporizes Rock")
+    
+    elif userAnswer == "Paper":
+        if(randomNumber == 1):
+            print("Scissors cuts Paper")
+        else:
+            print("Lizard eats Paper")
+    
+    elif userAnswer == "Scissors":
+        if(randomNumber == 1):
+            print("Rock crushes Scissors")
+        else:
+            print("Spock smashes Scissors")
+    
+    elif userAnswer == "Lizard":
+        if(randomNumber == 1):
+            print("Rock Smashes Lizard")
+        else:
+            print("Scissors decapitates Lizard")
+    
+    elif userAnswer == "Spock":
+        if(randomNumber == 1):
+            print("Sizard poisons Spock")
+        else:
+            print("Paper disproves Spock")
+
+    else:
+        print("who knows")
 
 while True:
-    userAnswer = input("What do you choose? (Rock, Paper, Scissors)? ")
+    answer = createAnswer()
+    print(answer)
+    computerAnswer(answer)
+    time.sleep(0.5)
+    
 
-    if userAnswer == "rock":
-        print("Paper wraps rock")
-    elif userAnswer == "paper":
-        print ("Scissors cuts paper")
-    elif userAnswer == "scissors":
-        print("Rock crushes scissors")
 
-    playAgain = input("Play again? (Y/N): ")
-    if playAgain != "y":
-        break
+#while True:
+ #   userAnswer = input("What do you choose? (Rock, Paper, Scissors, Lizard, Spock)? (Or type exit to quit) ")
+  #  if userAnswer == "exit":
+   #     break
+    #computerAnswer(userAnswer)
